c85a6d343f8e0d525a7aa55a38c08241c88ed7fb
Nicholas Car <nick@kurrawong.ai>
2024-12-17 16:52:06 +1000

    update test documentor to use demo-vocabs test data

--- manifest/tests/test_documentor.py
+++ manifest/tests/test_documentor.py
@@ -10,19 +10,23 @@ def test_create_table_01():
         Resource | Role | Description
         --- | --- | ---
         Catalogue Definition, [`catalogue.ttl`](catalogue.ttl) | [Container Data](https://prez.dev/ManifestResourceRoles/ContainerData) | The definition of, and medata for, the container which here is a dcat:Catalog object
-        Content, [`vocabs/*.ttl`](vocabs/*.ttl) | [Content Data](https://prez.dev/ManifestResourceRoles/ContentData) | skos:ConceptsScheme objects in RDF (Turtle) files in the vocabs/ folder
+        Content Data, [`vocabs/*.ttl`](vocabs/*.ttl) | [Content Data](https://prez.dev/ManifestResourceRoles/ContentData) | skos:ConceptsScheme objects in RDF (Turtle) files in the vocabs/ folder
         Profile Definition, [`ogc_records_profile.ttl`](https://github.com/RDFLib/prez/blob/main/prez/reference_data/profiles/ogc_records_profile.ttl) | [Container & Content Model](https://prez.dev/ManifestResourceRoles/ContainerAndContentModel) | The default Prez profile for Records API
-        Labels file, [`_background/labels.ttl`](_background/labels.ttl) | [Complete Content and Container Labels](https://prez.dev/ManifestResourceRoles/CompleteContainerAndContentLabels) | An RDF file containing all the labels for the container content                
+        Labels, [`_background/labels.ttl`](_background/labels.ttl) | [Complete Content and Container Labels](https://prez.dev/ManifestResourceRoles/CompleteContainerAndContentLabels) | An RDF file containing all the labels for the container content                
         """).strip()
 
-    g = Graph().parse(Path(__file__).parent / "manifest-01-valid.ttl")
+    g = Graph().parse(Path(__file__).parent / "demo-vocabs" / "manifest.ttl")
 
     result = create_table(g)
 
     print()
+    print()
     print(expected)
     print()
+    print()
     print(result)
+    print()
+    print()
 
     assert result == expected
 
@@ -37,7 +41,7 @@ def test_create_table_02():
         Labels file, [`_background/labels.ttl`](_background/labels.ttl) | [Complete Content and Container Labels](https://prez.dev/ManifestResourceRoles/CompleteContainerAndContentLabels) | An RDF file containing all the labels for the container content                
         """).strip()
 
-    g = Graph().parse(Path(__file__).parent / "manifest-02-invalid.ttl")
+    g = Graph().parse(Path(__file__).parent / "demo-vocabs" / "manifest-invalid-01.ttl")
 
     with pytest.raises(ValueError):
-        result = create_table(g)
+        create_table(g)
