d43aea2a6ce8201725e047e85ac262fca7c2e475
Nicholas Car <nick@kurrawong.ai>
2024-12-28 16:08:32 +1000

    moved manifext to github.com/kurrawong/prez-manifest

--- manifest/tests/test_loader.py
+++
@@ -1,9 +0,0 @@
-from manifest.loader import load
-from pathlib import Path
-
-
-def test_load_01():
-    manifest = Path(__file__).parent / "demo-vocabs" / "manifest.ttl"
-    results_file = Path(__file__).parent / "results.trig"
-    print()
-    print(load(manifest, sparql_endpoint=None, export_quads_file=results_file))
